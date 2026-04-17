5430d99bbb8bc86c50beab014058fcdfc8954fc2
gabrielclavila <gabriel.clavila@gmail.com>
2026-04-17 16:13:09 -0400

    Adicionando a notação PostMapping no método do Controller de Pedido e alterando a classe Pedido já preencher alguns dados que vão ser retornados na requisição

--- pedidos-api/src/main/java/com/br/gabrielclavila/pedidos/api/controller/PedidoController.java
+++ pedidos-api/src/main/java/com/br/gabrielclavila/pedidos/api/controller/PedidoController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class PedidoController {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
 
+    @PostMapping
     public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido){
         logger.info("Pedido recebido: {}", pedido.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(pedido);

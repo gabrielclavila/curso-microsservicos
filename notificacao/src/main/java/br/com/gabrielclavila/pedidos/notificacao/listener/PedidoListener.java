2f615c7a8aad7aa591d73ffe9cfad06361862d00
gabrielclavila <gabriel.clavila@gmail.com>
2026-05-05 10:51:33 -0400

    Alteracao no PedidoListener para incluir a injecao de dependencia do EmailService e poder consumir as requisicoes da fila do rabbitmq e enviar a notificacao via e-mail

--- notificacao/src/main/java/br/com/gabrielclavila/pedidos/notificacao/listener/PedidoListener.java
+++ notificacao/src/main/java/br/com/gabrielclavila/pedidos/notificacao/listener/PedidoListener.java
@@ -1,6 +1,7 @@
 package br.com.gabrielclavila.pedidos.notificacao.listener;
 
 import br.com.gabrielclavila.pedidos.notificacao.entity.Pedido;
+import br.com.gabrielclavila.pedidos.notificacao.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,8 +12,15 @@ public class PedidoListener {
 
     public final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
 
+    private final EmailService emailService;
+
+    public PedidoListener(EmailService emailService){
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-notificacao")
     public void enviarNotificacao(Pedido pedido){
+        emailService.enviarEmail(pedido);
         logger.info("Notificação gerada: {}", pedido.toString());
     }
 }

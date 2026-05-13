9fe12f1a5d5ed5a377976d469c47e82a9eb0d9d4
gabrielclavila <gabriel.clavila@gmail.com>
2026-05-13 10:45:15 -0400

    Criação dos repositories e da classe de configuração do rabbitMq no projeto processador, e configuracao dos parametros do rabbitmq no arquivo application.properties

---
+++ processador/src/main/java/br/com/gabrielclavila/pedidos/processador/repository/ItemPedidoRepository.java
@@ -0,0 +1,12 @@
+package br.com.gabrielclavila.pedidos.processador.repository;
+
+import br.com.gabrielclavila.pedidos.processador.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, UUID> {
+
+}
---
+++ processador/src/main/java/br/com/gabrielclavila/pedidos/processador/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package br.com.gabrielclavila.pedidos.processador.repository;
+
+import br.com.gabrielclavila.pedidos.processador.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+
+}

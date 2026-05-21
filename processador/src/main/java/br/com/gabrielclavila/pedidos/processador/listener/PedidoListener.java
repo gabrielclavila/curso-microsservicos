0a61980216d4d9e10cf43bbf50fb380e04de6846
gabrielclavila <gabriel.clavila@gmail.com>
2026-05-21 16:09:43 -0400

    Alterção na classe PedidoListener para fazer a injeção de dependencia do PedidoService para poder salvar as informações do pedido no banco de dados postgresql

--- processador/src/main/java/br/com/gabrielclavila/pedidos/processador/listener/PedidoListener.java
+++ processador/src/main/java/br/com/gabrielclavila/pedidos/processador/listener/PedidoListener.java
@@ -2,6 +2,7 @@ package br.com.gabrielclavila.pedidos.processador.listener;
 
 import br.com.gabrielclavila.pedidos.processador.entity.Pedido;
 import br.com.gabrielclavila.pedidos.processador.entity.enums.Status;
+import br.com.gabrielclavila.pedidos.processador.service.PedidoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,9 +13,16 @@ public class PedidoListener {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
 
+    private final PedidoService pedidoService;
+
+    //Injeção de Dependência por meio do construtor da classe
+    public PedidoListener(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
     @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-processamento")
     private void salvarPedido(Pedido pedido){
         pedido.setStatus(Status.PROCESSADO);
-        logger.info("Pedido processado: {}", pedido.toString());
+        pedidoService.save(pedido);
     }
 }

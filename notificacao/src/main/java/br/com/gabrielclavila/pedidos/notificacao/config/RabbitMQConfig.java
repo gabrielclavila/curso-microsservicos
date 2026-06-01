e77740de85f12600517207bbd4aff3d7db2d483f
gabrielclavila <gabriel.clavila@gmail.com>
2026-06-01 15:16:36 -0400

    Alterção no application.properties e no RabbitMQConfig do projeto de notificacao para configurar a fila de pedidos não processados no RabbitMQ

--- notificacao/src/main/java/br/com/gabrielclavila/pedidos/notificacao/config/RabbitMQConfig.java
+++ notificacao/src/main/java/br/com/gabrielclavila/pedidos/notificacao/config/RabbitMQConfig.java
@@ -15,6 +15,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitMQConfig {
 
@@ -24,19 +27,42 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private String queueName;
 
+    @Value("${rabbitmq.exchange.dlx.name}")
+    private String exchangeDlxName;
+
+    @Value("${rabbitmq.queue.dlq.name}")
+    private String queueDlqName;
+
     @Bean
     public FanoutExchange pedidosExchange(){
         return new FanoutExchange(exchangeName);
     }
 
     @Bean
+    public FanoutExchange pedidosDlxExchange(){
+        return new FanoutExchange(exchangeDlxName);
+    }
+
+    @Bean
     public Queue notificaoQueue(){
-        return new Queue(queueName);
+        Map<String, Object> argumentos = new HashMap<>();
+        argumentos.put("x-dead-letter-exchange", exchangeDlxName);
+        return new Queue(queueName,true,false,false, argumentos);
+    }
+
+    @Bean
+    public Queue notificaoDlqQueue(){
+        return new Queue(queueDlqName);
     }
 
     @Bean
     public Binding binding(){
         return BindingBuilder.bind(notificaoQueue()).to(pedidosExchange());
+    }
+
+    @Bean
+    public Binding bindingDlq(){
+        return BindingBuilder.bind(notificaoDlqQueue()).to(pedidosDlxExchange());
     }
 
     @Bean
